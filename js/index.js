2a9b3d9522c86d77da75752d9ca2fcec6163bea4
cgiraldovi <72058741+cgiraldovi@users.noreply.github.com>
2022-07-27 16:33:07 -0500

    Tercer commit, se han agregado las clases y se ha vuelto a modificar un poco el css, aun sigue existiendo el error del borde del div father

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const ingresos = [
+    new Ingreso("salario", 2100.00),
+    new Egreso("venta coche", 1500)
+];
+
+const egresos = [
+    new Egreso("venta departamento", 900),
+    new Egreso("Ropa", 400)
+]
